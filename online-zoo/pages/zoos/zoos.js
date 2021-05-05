e8645a2d4b92c57039bca214ce74bacb12a76cff
lyana <lyana-m@yandex.ru>
2021-05-05 17:12:47 +0500

    feat: add video slider

--- online-zoo/pages/zoos/zoos.js
+++ online-zoo/pages/zoos/zoos.js
@@ -1,9 +1,3 @@
-// const slides = document.querySelectorAll('.carousel__item');
-
-// slides.forEach(slide => slide.addEventListener('click', (event) => {
-
-// }));
-
 const plugs = document.querySelectorAll('.plug');
 const cards = document.querySelectorAll('.sidebar .carousel__item');
 
@@ -16,39 +10,35 @@ plugs.forEach(plug => plug.addEventListener('click', (event) => {
   previewVideo.src = mainVideoSrc;
 }));
 
-// cards.forEach(card => card.addEventListener('click', (event) => {
-    
-//     const cardId = event.currentTarget.getAttribute('id');
-//     console.log(cardId);
-//     const link = document.createElement('a');
-//     link.href = `../${cardId}/${cardId}.html`;
-//     link.click();
-//     link.remove();
-    
-// }));
 
-// const ifames = document.querySelectorAll('iframe');
-// console.log(ifames);
+const dots = document.querySelectorAll('.switcher__dot');
+let pressed = false;
 
+dots.forEach(dot => dot.addEventListener('click', (event) => {
+  const dotActive = document.querySelector('.switcher__dot_active');
+  const track = document.querySelector('.preview-video-track');
+  const slider = document.querySelector('.broadcast__preview-video');
+  const slide = document.querySelector('.preview-video__item');
+  const sliderWidth = slider.offsetWidth;
+  const slideWidth = slide.offsetWidth;
+  let offset = 0;
 
-// document.documentElement.onload = () => {
-//   cards.forEach(card => card.addEventListener('click', (event) => {
-//     const cardId = event.currentTarget.getAttribute('id');
-//     console.log(cardId);
-//     const link = document.createElement('a');
-//     link.href = `../${cardId}/${cardId}.html`;
-//     link.click();
-//     link.remove();
-//   }));
-// };
-
-// document.addEventListener('load', () => {
-//   cards.forEach(card => card.addEventListener('click', (event) => {
-//     const cardId = event.currentTarget.getAttribute('id');
-//     console.log(cardId);
-//     const link = document.createElement('a');
-//     link.href = `../${cardId}/${cardId}.html`;
-//     link.click();
-//     link.remove();
-//   }));
-// });
+  dotActive.classList.remove('switcher__dot_active');
+  event.target.classList.add('switcher__dot_active');  
+  
+  if (event.target === dots[0]) {
+    track.style.transform = `translateX(0)`;    
+  }
+  if (event.target === dots[1]) {
+    track.style.transform = `translateX(-${slideWidth * 3}px)`;      
+  }  
+  if (event.target === dots[1] && pressed === true) {
+    track.style.transform = `translateX(-${slideWidth}px)`;
+    pressed = false;    
+  }
+  if (event.target === dots[2]) {
+    track.style.transform = `translateX(-${slideWidth * 4}px)`;
+    pressed = true;    
+  }
+  
+}));

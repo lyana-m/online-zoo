51e8980f9095595d180030953ba2f5c5b5f51ef5
lyana <lyana-m@yandex.ru>
2021-05-05 16:12:33 +0500

    feat: add link on zoos cards

--- online-zoo/pages/zoos/zoos.js
+++ online-zoo/pages/zoos/zoos.js
@@ -5,12 +5,50 @@
 // }));
 
 const plugs = document.querySelectorAll('.plug');
+const cards = document.querySelectorAll('.sidebar .carousel__item');
 
 plugs.forEach(plug => plug.addEventListener('click', (event) => {
   const previewVideo = event.target.previousElementSibling;
   const previewVideoSrc = previewVideo.src;
   const mainVideo = document.querySelector('.broadcast__main-video iframe');
-  const mainVideoSrc = mainVideo.src;  
+  const mainVideoSrc = mainVideo.src;
   mainVideo.src = previewVideoSrc;
   previewVideo.src = mainVideoSrc;
 }));
+
+// cards.forEach(card => card.addEventListener('click', (event) => {
+    
+//     const cardId = event.currentTarget.getAttribute('id');
+//     console.log(cardId);
+//     const link = document.createElement('a');
+//     link.href = `../${cardId}/${cardId}.html`;
+//     link.click();
+//     link.remove();
+    
+// }));
+
+// const ifames = document.querySelectorAll('iframe');
+// console.log(ifames);
+
+
+// document.documentElement.onload = () => {
+//   cards.forEach(card => card.addEventListener('click', (event) => {
+//     const cardId = event.currentTarget.getAttribute('id');
+//     console.log(cardId);
+//     const link = document.createElement('a');
+//     link.href = `../${cardId}/${cardId}.html`;
+//     link.click();
+//     link.remove();
+//   }));
+// };
+
+// document.addEventListener('load', () => {
+//   cards.forEach(card => card.addEventListener('click', (event) => {
+//     const cardId = event.currentTarget.getAttribute('id');
+//     console.log(cardId);
+//     const link = document.createElement('a');
+//     link.href = `../${cardId}/${cardId}.html`;
+//     link.click();
+//     link.remove();
+//   }));
+// });
